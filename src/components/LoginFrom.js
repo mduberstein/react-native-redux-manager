ec556f09eb3df74c0cb32e2d1a660ae86c19aa7f
Michael Duberstein <mduberst@amazon.com>
2019-10-14 14:53:37 -0400

    After Clip 364 Handling Form Updates wth Action Creators

--- src/components/LoginFrom.js
+++ src/components/LoginFrom.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {Card, CardSection, Input, Button} from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text){
+
+  }
+
   render() {
     return (
       <Card>
@@ -9,7 +13,8 @@ class LoginForm extends Component {
           <Input label="Email" placeholder="email@gmail.com" />
         </CardSection>
         <CardSection>
-          <Input secureTextEntry label="Password" placeholder="password" />
+          <Input secureTextEntry label="Password" placeholder="password"
+          onChangeText={this.onEmailChange.bind(this)}/>
         </CardSection>
         <CardSection>
           <Button>Login</Button>
