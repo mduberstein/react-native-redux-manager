13700d0c0d5d3757e8aca80119364d4aec4b3947
Michael Duberstein <mduberst@amazon.com>
2019-10-14 16:00:15 -0400

    After Clip 366: Typed Actions

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,12 @@
+import {EMAIL_CHANGED} from '../actions/types';
 const INITIAL_STATE = {email: ''};
+
 export default (state = INITIAL_STATE, action) => {
-  switch(action.type){
-    default: 
-    return state;
+  switch (action.type) {
+    case EMAIL_CHANGED:
+      
+    default:
+      return state;
   }
 };
+ 
